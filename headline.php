d35be173b3266e16c6ad00dafc92af25b5ab26a9
Agus Supriatman <agus28081991@gmail.com>
2014-05-04 23:32:14 +0700

    Update headline.php

--- headline.php
+++ headline.php
@@ -17,7 +17,7 @@
 $html = "";
 $url = "http://www.goal.com/id-ID/feeds/team-news?id=124&fmt=rss&ICID=CP_124"; 
 $xml = simplexml_load_file($url);
-for($i = 0; $i < 10; $i++){
+for($i = 0; $i < 2; $i++){
 
 $title = $xml->channel->item[$i]->title;
 $link = $xml->channel->item[$i]->link;
@@ -32,6 +32,39 @@ echo $html;
 ?>
 		</td>
 		<td width="20%"></td>
+	</tr>
+	<tr>
+		<td><center><img alt="logorealmadrid" src="logoreal.gif" height="20%" width="20%"></center></td>
+		<td>
+			<h1>Dibuat oleh :</h1>
+			<table>
+				<tr>
+					<td>
+		<?php
+			$dom= new DomDocument("1.0");
+			$dom ->load("sumber.xml");
+			$tampilnama =$dom->getElementsByTagName("nama");
+			for ($i=0; $i<$tampilnama->length; $i++)
+			{
+				echo $tampilnama->item($i)->nodeValue."<br/>";
+			}
+		?>
+					</td>
+					<td>
+		<?php
+			$dom= new DomDocument("1.0");
+			$dom ->load("sumber.xml");
+			$tampilnpm =$dom->getElementsByTagName("npm");
+			for ($i=0; $i<$tampilnpm->length; $i++)
+			{
+				echo $tampilnpm->item($i)->nodeValue."<br/>";
+			}
+		?>
+					</td>
+			</tr>
+		</table>
+		</td>
+		<td></td>
 	</tr>
 </table>
 </center>
