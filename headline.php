d1db7e414b255a07d510547241806b62147ec8b0
Karina Nursyafani Adhi <Larcnachiel26@gmail.com>
2014-05-04 21:38:58 -0700

    Update headline.php
    mengganti

--- headline.php
+++ headline.php
@@ -3,12 +3,12 @@
 <body background="197042_431320693589160_999032084_n.jpg">
 <center>
 <title> Real Madrid Headline News </title>
-<table width="553" align="center" bgcolor="#FFFFCC">
+<table width="553" height="25" align="center" bgcolor="#000066">
 <tr>
-<td><font color="#FF3300" face="><marquee direction="left"><h1><p align="center">REAL MADRID NEWS</h1></p></marquee></font></td>
+<td><font size="5" color="#FFFFFF" face="Lucida Sans Unicode, Lucida Grande, sans-serif"><p align="center">REAL MADRID NEWS</p></font></td>
 
-</tr></table>
-<table width="518">
+</tr></table><br><br>
+<table width="518" bgcolor="#7591DB">
 	<tr>
 		<td></td>
 		<td><center><img alt="logorealmadrid" src="logoreal.gif" height="20%" width="20%"></center></td>
